5ee20c76de4af0d77a8127aec528f996274b7e9a
sarmelano <sarmelano.main@gmail.com>
2024-07-16 14:23:03 -0700

    signup & login routes created

--- lib/utils/api-routes.ts
+++ lib/utils/api-routes.ts
@@ -1,4 +1,6 @@
 import { Db, MongoClient } from 'mongodb'
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcryptjs'
 import { shuffle } from './common'
 
 export const getDbAndReqBody = async (
@@ -34,3 +36,47 @@ export const getNewAndBestsellerGoods = async (db: Db, fieldName: string) => {
       .slice(0, 2),
   ])
 }
+
+export const generateTokens = (name: string, email: string) => {
+  const accessToken = jwt.sign(
+    {
+      name,
+      email,
+    },
+    process.env.NEXT_PUBLIC_ACCESS_TOKEN_KEY as string,
+    {
+      expiresIn: '10m',
+    }
+  )
+
+  const rofreshToken = jwt.sign(
+    {
+      email,
+    },
+    process.env.NEXT_PUBLIC_REFRESH_TOKEN_KEY as string,
+    { expiresIn: '10m' }
+  )
+
+  return { accessToken, rofreshToken }
+}
+
+export const createUserAndGenerateTokens = async (
+  db: Db,
+  reqBody: { name: string; password: string; email: string }
+) => {
+  const salt = bcrypt.genSaltSync(10)
+  const hash = bcrypt.hashSync(reqBody.password, salt)
+
+  await db.collection('users').insertOne({
+    name: reqBody.name,
+    password: hash,
+    email: reqBody.email,
+    image: '',
+    role: 'user',
+  })
+
+  return generateTokens(reqBody.name, reqBody.email)
+}
+
+export const findUserByEmail = async (db: Db, email: string) =>
+  db.collection('users').findOne({ email })
