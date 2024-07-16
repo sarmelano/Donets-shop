7d57e385d8395672bffff79d63aa562d9eeec3ae
sarmelano <sarmelano.main@gmail.com>
2024-07-16 14:23:41 -0700

    login route imports fixed

---
+++ app/api/users/login/route.ts
@@ -0,0 +1,29 @@
+import bcrypt from 'bcryptjs'
+import { NextResponse } from 'next/server'
+import clientPromise from '@/lib/mongodb'
+import {
+  findUserByEmail,
+  generateTokens,
+  getDbAndReqBody,
+} from '@/lib/utils/api-routes'
+
+export async function POST(req: Request) {
+  const { db, reqBody } = await getDbAndReqBody(clientPromise, req)
+  const user = await findUserByEmail(db, reqBody.email)
+
+  if (!user) {
+    return NextResponse.json({
+      warningMessage: 'User does not exist',
+    })
+  }
+
+  if (!bcrypt.compareSync(reqBody.password, user.password)) {
+    return NextResponse.json({
+      warningMessage: 'Incorrect login or password!',
+    })
+  }
+
+  const tokens = generateTokens(user.name, reqBody.email)
+
+  return NextResponse.json(tokens)
+}
