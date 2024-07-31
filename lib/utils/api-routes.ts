1e3e28f2b41b06a752dab0fae8e6fe086fb9563b
sarmelano <sarmelano.main@gmail.com>
2024-07-30 21:51:56 -0700

    db cart collection added

--- lib/utils/api-routes.ts
+++ lib/utils/api-routes.ts
@@ -2,6 +2,7 @@ import { Db, MongoClient } from 'mongodb'
 import jwt, { VerifyErrors } from 'jsonwebtoken'
 import bcrypt from 'bcryptjs'
 import { shuffle } from './common'
+import { NextResponse } from 'next/server'
 
 export const getDbAndReqBody = async (
   clientPromise: Promise<MongoClient>,
@@ -132,3 +133,27 @@ export const isValidAccessToken = async (token: string | undefined) => {
 
 export const parseJwt = (token: string) =>
   JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString())
+
+export const getDataFromDBByCollection = async (
+  clientPromise: Promise<MongoClient>,
+  req: Request,
+  collection: string
+) => {
+  const { db, validatedTokenResult, token } = await getAuthRouteData(
+    clientPromise,
+    req,
+    false
+  )
+
+  if (validatedTokenResult.status !== 200) {
+    return NextResponse.json(validatedTokenResult)
+  }
+
+  const user = await findUserByEmail(db, parseJwt(token as string).email)
+  const items = await db
+    .collection(collection)
+    .find({ userId: user?._id })
+    .toArray()
+
+  return NextResponse.json(items)
+}
---
+++ types/cart.ts
@@ -0,0 +1,14 @@
+export interface ICartItem {
+  _id: string
+  clientId: string
+  userId: string
+  productId: string
+  image: string
+  name: string
+  size: string
+  count: string | number
+  price: string
+  totalPrice: string
+  inStock: string
+  color: string
+}
