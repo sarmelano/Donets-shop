1dda062dd23141c77d6d1555743e6dbe66852e94
sarmelano <sarmelano.main@gmail.com>
2024-08-31 23:03:13 -0700

    fixed currency fake

--- components/elements/NonexistentCurrency/NonexistentCurrency.tsx
+++ components/elements/NonexistentCurrency/NonexistentCurrency.tsx
@@ -1,13 +1,3 @@
-import Image from 'next/image'
-
-const NonexistentCurrency = () => (
-  <Image
-    src='/img/NonexistentCurrency.svg'
-    alt='Non-existent Currency'
-    className='nonexistentCurrency'
-    width={20}
-    height={20}
-  />
-)
+const NonexistentCurrency = () => <span className='nonexistentCurrency'>$</span>
 
 export default NonexistentCurrency
