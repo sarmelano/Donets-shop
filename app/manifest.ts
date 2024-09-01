8051346e2b6ca73839e97a491ac0a6617ac3f0f1
sarmelano <sarmelano.main@gmail.com>
2024-08-31 21:55:52 -0700

    manifest added

--- app/manifest.ts
+++ app/manifest.ts
@@ -4,7 +4,18 @@ export default function manifest(): MetadataRoute.Manifest {
   return {
     name: 'Donets Application',
     short_name: 'Donets App',
-    description: 'Donets shop project',
+    description: 'Donets shop pet project',
     start_url: '/',
+    background_color: '#fff',
+    theme_color: '#fff',
+    display: 'standalone',
+    icons: [
+      {
+        src: '/img/icon.svg',
+        sizes: '196x196 512x512 144x144 192x192 128x128 120x120 180x180',
+        type: 'image/svg',
+        purpose: 'maskable',
+      },
+    ],
   }
 }
